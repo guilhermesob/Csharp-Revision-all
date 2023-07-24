0b9e17b5ec2035633f05771939f4293f486322e0
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-24 12:10:51 -0300

    Create hellowin.cs

---
+++ hellowin.cs
@@ -0,0 +1,39 @@
+HWND hwnd = CreateWindowsEx(
+	0, 
+	CLASS_NAME, 
+	L"Hello THM!", 
+	WS_OVERLAPPEDWINDOW, 
+	CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+	NULL, 
+	NULL, 
+	hInstance, 
+	NULL
+	);
+
+BOOL Create(
+        PCWSTR lpWindowName,
+        DWORD dwStyle,
+        DWORD dwExStyle = 0,
+        int x = CW_USEDEFAULT,
+        int y = CW_USEDEFAULT,
+        int nWidth = CW_USEDEFAULT,
+        int nHeight = CW_USEDEFAULT,
+        HWND hWndParent = 0,
+        HMENU hMenu = 0
+        )
+    {
+        WNDCLASS wc = {0};
+
+        wc.lpfnWndProc   = DERIVED_TYPE::WindowProc;
+        wc.hInstance     = GetModuleHandle(NULL);
+        wc.lpszClassName = ClassName();
+
+        RegisterClass(&wc);
+
+        m_hwnd = CreateWindowEx(
+            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
+            );
+
+        return (m_hwnd ? TRUE : FALSE);
+    }
