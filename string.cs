a53d0beaafe9106eab28320080b09c44b3573036
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-08-03 23:16:24 -0300

    Create string.cs

---
+++ string.cs
@@ -0,0 +1,15 @@
+// C# program to illustrate String
+using System;
+  
+class GFG{
+  
+public static void Main()
+{
+      
+    // Declare String variable
+    String b = "Welcome to GeeksforGeeks";
+  
+    // Display the result
+    Console.WriteLine(a);
+}
+}
